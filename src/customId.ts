3bb6ec9791ce632118f9c048c04cde1f35e9a7eb
Andrei Morozov <oficialmorozov@gmail.com>
2022-11-08 21:26:30 +0000

    Remove zod

--- src/customId.ts
+++ src/customId.ts
@@ -1,31 +1,21 @@
-import { z, ZodObject } from "zod";
-
 const customIdMaxLength = 100;
 
-export class CustomId<TSchema extends ZodObject<any>> {
-  private readonly _prefixDataSeparator = ":";
-
-  constructor(
-    public readonly prefix: string,
-    public readonly schema?: TSchema
-  ) {}
+export class CustomId<TData extends Record<string, any> = Record<string, any>> {
+  constructor(public readonly prefix: string) {}
 
   public parse(customId: string) {
     const prefix = customId.slice(0, this.prefix.length);
-    const data = customId.substring(
-      this.prefix.length + this._prefixDataSeparator.length
-    );
-
+    const data = customId.substring(this.prefix.length);
     return { prefix, data };
   }
 
-  public pack(data?: z.infer<TSchema>) {
+  public pack(data?: TData): string {
     if (!data) {
       return this.prefix;
     }
 
     const dataJson = JSON.stringify(data);
-    const packedData = `${this.prefix}${this._prefixDataSeparator}${dataJson}`;
+    const packedData = `${this.prefix}${dataJson}`;
 
     if (packedData.length > customIdMaxLength) {
       throw new Error("Too long custom id");
@@ -34,18 +24,18 @@ export class CustomId<TSchema extends ZodObject<any>> {
     return packedData;
   }
 
-  public unpack(customId: string): z.infer<TSchema> {
+  public unpack(customId: string): TData | undefined {
     const { prefix, data } = this.parse(customId);
 
     if (prefix !== this.prefix) {
       throw new Error("Invalid payload");
     }
 
-    if (!this.schema) {
-      return {};
+    if (!data.length) {
+      return undefined;
     }
 
     const unpackedData = JSON.parse(data);
-    return this.schema.parse(unpackedData);
+    return unpackedData;
   }
 }
