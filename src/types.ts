c8190d1ac2d21f8dbd75419c95a013d88e7cfd26
Andrei Morozov <oficialmorozov@gmail.com>
2022-11-09 03:33:16 +0000

    Update types

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@ import { BaseSlashCommand } from "./command";
 import { BaseExtension } from "./extension";
 
 type Subclass<T extends abstract new (...args: any[]) => any> = new (
-  ...args: ConstructorParameters<T>
+  ...args: any[]
 ) => InstanceType<T>;
 
-export type CommandSubclass = Subclass<typeof BaseSlashCommand>;
+export type CommandSubclass = Subclass<typeof BaseSlashCommand<any>>;
 export type ExtensionSubclass = Subclass<typeof BaseExtension>;
