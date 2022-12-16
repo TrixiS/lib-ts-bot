e238d7e1fdd50417250b487529a24907d229efd2
TrixiS <oficialmorozov@gmail.com>
2022-12-16 04:28:05 +0300

    Remove component specified event checks

--- src/checks/eventHandlerChecks.ts
+++ src/checks/eventHandlerChecks.ts
@@ -1,21 +1,6 @@
 import { CustomId } from "../customId";
 import { eventHandlerCheckFactory } from "./checkFactory";
 
-export const buttonInteractionHandler = () =>
-  eventHandlerCheckFactory<"interactionCreate">(async (interaction) => {
-    return interaction.isButton();
-  });
-
-export const selectMenuInteractionHandler = () =>
-  eventHandlerCheckFactory<"interactionCreate">(async (interaction) => {
-    return interaction.isSelectMenu();
-  });
-
-export const modalSubmitInteractionHandler = () =>
-  eventHandlerCheckFactory<"interactionCreate">(async (interaction) => {
-    return interaction.isModalSubmit();
-  });
-
 export const checkCustomId = (customId: CustomId<any>) =>
   eventHandlerCheckFactory<"interactionCreate">(async (interaction) => {
     if (!interaction.isMessageComponent() && !interaction.isModalSubmit()) {
